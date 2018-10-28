ef4bd80a50154a1d57aa1ba719a11485cf744470
Dhruveen <dnp49@njit.edu>
2018-10-28 14:09:46 -0400

    Task : Factory created for Car & set the make field

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Car::class, function (Faker $faker) {
+    $random = mt_rand(0,2);
+    $CarsArray = array("Ford", "Honda", "Toyota");
+    return [
+        'make' => $CarsArray[$random],
+        'model' => $faker->company,
+        'year' => $faker->year,
+    ];
+});
