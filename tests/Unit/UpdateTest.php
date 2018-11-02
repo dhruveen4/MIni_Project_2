759243a07a43c322ea8d211508a40cf6255ddd8a
Dhruveen <dnp49@njit.edu>
2018-11-02 01:03:35 -0400

    Task Testing 6: Created unit test to Update User in database

--- tests/Unit/UpdateTest.php
+++ tests/Unit/UpdateTest.php
@@ -6,18 +6,19 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
-use App\Car;
 
-class UserTest extends TestCase
+class UpdateTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testInsertUser()
+    public function testExample()
     {
-        $user = factory(\App\User::class)->create();
-        dd($user);
+        $user = User::find(1);
+        $user->name = 'steve smith';
+
+        $this->assertTrue($user->save());
     }
 }
