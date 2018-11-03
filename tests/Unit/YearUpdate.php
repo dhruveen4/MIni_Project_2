672fc8e5e0bec3b63d60a267edc6e230dbddd4ed
Dhruveen <dnp49@njit.edu>
2018-11-02 21:05:46 -0400

    Task Testing 10 : Year Updated

---
+++ tests/Unit/YearUpdate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class YearUpdate extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $car = Car::find(1);
+        $car->year = '2000';
+
+        $this->assertTrue($car->save());
+    }
+}
